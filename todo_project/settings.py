4db22ae6a605618394723f3ba3e12df5248c8d74
ahamed14051 <ma00087@mix.wvu.edu>
2024-09-05 01:42:30 -0400

    Initial commit

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -71,25 +71,25 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 # todo_project/settings.py
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todo',    # Replace with your database name
-        'USER': 'postgres',        # Replace with your database user
-        'PASSWORD': '123',# Replace with your database password
-        'HOST': 'localhost',     # Replace with your database host (default: 'localhost')
-        'PORT': '5432',          # Replace with your database port (default: '5432')
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'todo',    # Replace with your database name
+#         'USER': 'postgres',        # Replace with your database user
+#         'PASSWORD': '123',# Replace with your database password
+#         'HOST': 'localhost',     # Replace with your database host (default: 'localhost')
+#         'PORT': '5432',          # Replace with your database port (default: '5432')
+#     }
+# }
 
 
 
